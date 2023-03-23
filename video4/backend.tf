49cb77d13b19aaeb0deee8a299ff8e21496b38de
D-Emperor <93507606+D-Emperor@users.noreply.github.com>
2023-03-23 11:25:40 +0530

    Update backend.tf

--- video4/backend.tf
+++ video4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    access_key = "AKIAVQRCLW4SZ2EDILQ7"
-    secret_key = "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"
+    access_key = "AKIAVLQ7"
+    secret_key = "L22QMqJ"
     bucket     = "terraforms3teja" #bucket name
     key        = "sree.tfstate"
     region     = "ap-south-1"
